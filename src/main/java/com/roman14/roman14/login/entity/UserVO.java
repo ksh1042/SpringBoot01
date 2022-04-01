b951f6960ff5d79fd1d8811bd27c968052e39e12
= <roman14@naver.com>
2022-04-01 09:07:26 +0900

    사용자 정보 VO 주석 추가

--- src/main/java/com/roman14/roman14/login/entity/UserVO.java
+++ src/main/java/com/roman14/roman14/login/entity/UserVO.java
@@ -6,6 +6,13 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * <h2>UserVO</h2>
+ * <pre>
+ *   - TABLE : DEVELOP.TB_USER_INFO
+ *   - 사용자 정보 VO
+ * </pre>
+ */
 @Entity
 @Data
 @Table(name = "TB_USER_INFO")
@@ -15,24 +22,31 @@ public class UserVO
   @Column(name = "USER_ID")
   private String userId;
 
+  /** 암호화된 사용자 패스워드 */
   @Column(name = "PASSWORD")
-  private String password; // !!DEVELOP ONLY!!
+  private String password;
 
+  /** 암호화에 사용되는 salt */
   @Column(name = "SALT")
   private long salt;
 
+  /** 사용자 이름 */
   @Column(name = "FIRST_NAME")
   private String firstName;
 
+  /** 사용자 성(姓) */
   @Column(name = "LAST_NAME")
   private String lastName;
 
+  /** 사용자 성별 */
   @Column(name = "SEX")
   private String sex;
 
+  /** 사용자 생성 날짜 */
   @Column(name = "REGIST_DATE")
   private LocalDateTime registDate;
 
+  /** 사용자의 마지막으로 수정 날짜 */
   @Column(name = "MODIFY_DATE")
   private LocalDateTime modifyDate;
 
@@ -40,6 +54,7 @@ public class UserVO
   @Column(name = "IS_USE")
   private String isUse;
 
+  /** 로그인 시도 이력 */
   @OneToMany
   @JoinColumn(name = "USER_ID")
   private List<UserHistoryVO> userHistory;
